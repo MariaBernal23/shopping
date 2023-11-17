beb3c3c68f997fa813d209533cb0d75e34de6499
HaydeeGrande <143965227+HaydeeGrande@users.noreply.github.com>
2023-11-17 07:55:47 -0600

    Edicion de nosotros.dart

--- lib/nosotros.dart
+++ lib/nosotros.dart
@@ -21,6 +21,20 @@ class Inicio extends StatelessWidget {
                     fontSize: 25,
                     color: Colors.pink)),
             SizedBox(height: 20),
+            Text('Jenifer Milena Duran',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                    color: Colors.purple)),
+            CircleAvatar(
+                backgroundImage: AssetImage('images/imagen1.jpeg'),
+                radius: 130),
+            Text('jenifer.duran22@itca.edu.sv',
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 25,
+                    color: Colors.pink)),
+            SizedBox(height: 30),
 
           
           ],
