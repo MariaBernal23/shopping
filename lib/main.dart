4ec990d04917cacb796b658a4141299665a4ef52
Michelleiva <michel.montes22@itca.edu.sv>
2023-11-17 01:29:47 -0600

    agregando menú drawer

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,126 @@ class CartItem {
 
   CartItem({required this.product, required this.quantity});
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange[800],
+        title: Center(
+            child: Text(
+          'Shopping',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              _showCartDrawer(context);
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              child: Image.asset("img/food1.png"),
+            ),
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Cupones',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Image.asset(
+                    'img/cupon.png', // Reemplaza con la ruta de tu imagen
+                    height: 30, // Ajusta la altura según tus necesidades
+                    width: 30, // Ajusta el ancho según tus necesidades
+                  ),
+                ],
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CouponPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Productos',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                    ),
+                  ),
+                  Image.asset(
+                    'img/producto.png', // Reemplaza con la ruta de tu imagen
+                    height: 30, // Ajusta la altura según tus necesidades
+                    width: 30, // Ajusta el ancho según tus necesidades
+                  ),
+                ],
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return ProductCard(
+                  product: products[index],
+                  onAddToCart: () {
+                    addToCart(products[index]);
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
